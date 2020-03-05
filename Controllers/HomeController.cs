d1b94542ca1da3fa9680502d17aabf3f9f2d53c9
andriy makar <mak27arr@gmail.com>
2020-03-06 00:20:13 +0200

    finish commit

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,8 +1,13 @@
-﻿using System;
+﻿using Microsoft.Web.Services3.Messaging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Xml;
+using TestTask.Models;
 
 namespace TestTask.Controllers
 {
@@ -13,17 +18,47 @@ namespace TestTask.Controllers
             return View();
         }
 
-        public ActionResult About()
-        {
-            ViewBag.Message = "Your application description page.";
-
-            return View();
-        }
-
         public ActionResult Contact()
         {
             ViewBag.Message = "Your contact page.";
 
+            return View();
+        }
+
+        [HttpGet]
+        public ActionResult Login()
+        {
+            var isapi = new isapiSOAP.ICUTechClient();
+            isapi.Login(ViewBag, ViewBag, "");
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Login(Login ldata)
+        {
+            var isapi = new isapiSOAP.ICUTechClient();
+            string response = isapi.Login(ldata.login, ldata.password, Request.UserHostAddress);
+            var json = JObject.Parse(response);
+            if (json.ContainsKey("ResultCode"))
+            {
+                ViewBag.ResultCode = "-1";
+                ViewBag.Msg = json["ResultMessage"].ToString();
+                return View("Index");
+            }
+            ViewBag.ResultCode = "1";
+            ViewBag.Msg = json;
+            return View("Index");
+        }
+
+        [HttpGet]
+        public ActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Register(RegisterInfo data)
+        {
+            var isapi = new isapiSOAP.ICUTechClient();
+            ViewBag.Response = isapi.RegisterNewCustomer(data.email, data.password, data.firstName, data.lastName, data.mobile, data.countryID, data.aID, data.signupIP);
             return View();
         }
     }
---
+++ Models/RegisterInfo.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace TestTask.Models
+{
+    public class RegisterInfo
+    {
+        public string email { get; set; }
+        public string password { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string mobile { get; set; }
+        public int countryID { get; set; }
+        public int aID { get; set; }
+        public string signupIP { get; set; }
+
+    }
+}
